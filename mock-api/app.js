d9079de19758561815314c72258338fdc1449cc2
owenheyrman <owen.heyrman@student.ap.be>
2023-10-11 11:30:24 +0200

    create mock api (and temp test html)
    this websocket api gives one line from the csv file every second

---
+++ mock-api/app.js
@@ -0,0 +1,41 @@
+const WebSocket = require('ws');
+const fs = require('fs');
+const Papa = require('papaparse');
+
+const wss = new WebSocket.Server({ port: 8080 });
+
+let csvData = [];
+
+// Parsing the CSV file
+Papa.parse(fs.createReadStream('./smaller_dataset.csv'), {
+    header: true,
+    dynamicTyping: true,
+    complete: function(results) {
+        csvData = results.data;
+    }
+});
+
+wss.on('connection', (ws) => {
+    console.log('Client connected');
+
+    let lineIndex = 0;
+
+    const sendLine = setInterval(() => {
+        if (lineIndex < csvData.length) {
+            ws.send(JSON.stringify(csvData[lineIndex]));
+            lineIndex++;
+        } else {
+            clearInterval(sendLine);
+        }
+    }, 1000);
+
+    ws.on('message', (message) => {
+        console.log(`Received message: ${message}`);
+        ws.send('Message received');
+    });
+
+    ws.on('close', () => {
+        console.log('Client disconnected');
+        clearInterval(sendLine);  // Clear the interval upon disconnection to prevent sending data to closed connection.
+    });
+});
