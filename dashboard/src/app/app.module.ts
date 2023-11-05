ee9aa1563957e85148f2d6e3f35154d979319d1f
Koen-Schrijvers <koen.schrijvers@student.ap.be>
2023-11-05 15:35:11 +0100

    Add Dashboard component
    Made de dashboard background. chart components have to be inserted.

--- dashboard/src/app/app.module.ts
+++ dashboard/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { StatsblockComponent } from './statsblock/statsblock.component';
 import { HttpClientModule } from '@angular/common/http';
 import { LiveblockComponent } from './liveblock/liveblock.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     StatsblockComponent,
-    LiveblockComponent
+    LiveblockComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
