95582a98220e00658ba48e2f0e5b98bfd2044601
owenheyrman <owen.heyrman@student.ap.be>
2023-11-05 19:55:59 +0100

    make component for bar chart

--- dashboard/src/app/app.module.ts
+++ dashboard/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { StatsblockComponent } from './statsblock/statsblock.component';
 import { HttpClientModule } from '@angular/common/http';
 import { LiveblockComponent } from './liveblock/liveblock.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { BarChartVolumeDisplayer9000Component } from './dashboard/bar-chart-volume-displayer9000/bar-chart-volume-displayer9000.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     StatsblockComponent,
     LiveblockComponent,
-    DashboardComponent
+    DashboardComponent,
+    BarChartVolumeDisplayer9000Component
   ],
   imports: [
     BrowserModule,
---
+++ dashboard/src/app/dashboard/bar-chart-volume-displayer9000/bar-chart-volume-displayer9000.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bar-chart-volume-displayer9000',
+  templateUrl: './bar-chart-volume-displayer9000.component.html',
+  styleUrls: ['./bar-chart-volume-displayer9000.component.css']
+})
+export class BarChartVolumeDisplayer9000Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
